da7b7ef96904ceca43a16664bff02f45fe148532
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-02-23 12:47:27 +1100

    Update

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,11 @@
 import { AppProps } from "next/app";
-import { Provider } from "next-auth/client";
+import { SessionProvider } from "next-auth/react";
 
-export function MyApp({ Component, pageProps }: AppProps) {
+export function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
-        <Provider session={pageProps.session}>
+        <SessionProvider session={session}>
             <Component {...pageProps} />
-        </Provider>
+        </SessionProvider>
     );
 }
 
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,14 @@
-import { useSession, signIn, signOut } from "next-auth/client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Web() {
-    const [session, loading] = useSession();
+    const { data: session } = useSession();
 
     return (
         <div>
             <h1>Web</h1>
             <p>{JSON.stringify(session)}</p>
-            <button onClick={signIn}>Sign In</button>
-            <button onClick={signOut}>Sign Out</button>
+            <button onClick={() => signIn()}>Sign In</button>
+            <button onClick={() => signOut()}>Sign Out</button>
         </div>
     );
 }
