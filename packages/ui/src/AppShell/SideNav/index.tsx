0369a91c67d1149ed03d5be80414cfe72b61241c
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-02-24 22:34:52 +1100

    Added icons to nav links

--- packages/ui/src/AppShell/SideNav/index.tsx
+++ packages/ui/src/AppShell/SideNav/index.tsx
@@ -1,3 +1,4 @@
+import { Bucket, FileSearch } from "tabler-icons-react";
 import { NavLink } from "./NavLink";
 
 interface IProps {}
@@ -7,8 +8,8 @@ export function SideNav({}: IProps) {
         <div className="flex h-screen">
             <div className="bg-gray-50 w-64 py-5 px-10">
                 <div className="flex flex-col space-y-5">
-                    <NavLink text="Storage" href="#" />
-                    <NavLink text="Search" href="#" />
+                    <NavLink text="Storage" href="#" icon={<Bucket />} />
+                    <NavLink text="Search" href="#" icon={<FileSearch />} />
                 </div>
             </div>
         </div>
