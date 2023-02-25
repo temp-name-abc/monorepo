fba8aac76bfc0d172a231958ff7300bf5462fe7e
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-02-25 11:59:32 +1100

    Replaced navlink component with button component

--- packages/ui/src/AppShell/SideNav/index.tsx
+++ packages/ui/src/AppShell/SideNav/index.tsx
@@ -1,5 +1,5 @@
 import { Bucket, FileSearch } from "tabler-icons-react";
-import { NavLink } from "./NavLink";
+import { Button } from "../../Button";
 
 interface IProps {}
 
@@ -8,8 +8,12 @@ export function SideNav({}: IProps) {
         <div className="flex h-screen">
             <div className="bg-gray-50 w-64 py-5 px-10">
                 <div className="flex flex-col space-y-5">
-                    <NavLink text="Storage" href="#" icon={<Bucket />} />
-                    <NavLink text="Search" href="#" icon={<FileSearch />} />
+                    <Button variant="dull" type="link" href="#" icon={<Bucket />}>
+                        Storage
+                    </Button>
+                    <Button variant="dull" type="link" href="#" icon={<FileSearch />}>
+                        Search
+                    </Button>
                 </div>
             </div>
         </div>
--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
 interface IProps {
     children: any;
@@ -6,26 +7,30 @@ interface IProps {
     variant: "dull" | "accent";
     onClick?: (e: any) => any;
     href?: string;
+    icon?: ReactNode;
 }
 
-export function Button({ children, type, variant, onClick, href }: IProps) {
+export function Button({ children, type, variant, onClick, href, icon }: IProps) {
     const color = variant === "dull" ? "text-gray-400 bg-gray-100 hover:bg-gray-200" : "text-gray-50 bg-violet-600 hover:bg-violet-700";
+    const paddingY = icon ? "py-4" : "py-2";
 
-    const style = `font-medium px-4 py-2 ${color}`;
+    const style = `font-medium px-4 ${paddingY} ${color} flex items-center justify-between`;
 
     if (type === "button")
         return (
             <button className={style} onClick={onClick}>
                 {children}
+                {icon}
             </button>
         );
     else if (type === "link" && !!href)
         return (
             <Link className={style} onClick={onClick} href={href}>
                 {children}
+                {icon}
             </Link>
         );
-    else throw Error("Invalid props for element Button");
+    else throw Error("Invalid props for component 'Button'");
 }
 
 export default Button;
