f87d87b1c0b90279a21911aa1148d9f8ac81a2b2
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-02-27 19:22:17 +1100

    Snapshot

--- packages/ui-chat/src/links.tsx
+++ packages/ui-chat/src/links.tsx
@@ -2,7 +2,6 @@ import { DotsCircleHorizontal, Send, CreditCard } from "tabler-icons-react";
 
 export const links = [
     { href: "/chat/conversations", children: "Conversations", icon: <Send /> },
-    { href: "#", children: "Endpoints", icon: <DotsCircleHorizontal /> },
     { href: "/chat/billing", children: "Billing", icon: <CreditCard /> },
 ];
 
--- packages/ui-storage/src/links.tsx
+++ packages/ui-storage/src/links.tsx
@@ -2,7 +2,6 @@ import { CreditCard, Folders, Search } from "tabler-icons-react";
 
 export const links = [
     { href: "/storage/collections", children: "Collections", icon: <Folders /> },
-    { href: "#", children: "Search", icon: <Search /> },
     { href: "/storage/billing", children: "Billing", icon: <CreditCard /> },
 ];
 
