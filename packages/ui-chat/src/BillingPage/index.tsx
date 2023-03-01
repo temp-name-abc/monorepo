62b3bac71627ed0a3003afe12629b32c03b0f1af
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-03-01 23:58:15 +1100

    Updated billing page

--- packages/ui-chat/src/BillingPage/index.tsx
+++ packages/ui-chat/src/BillingPage/index.tsx
@@ -7,7 +7,7 @@ interface IProps {}
 export function BillingPage({}: IProps) {
     return (
         <SubAppShell title="Chat / Billing" description="Subscribe and manage your subscriptions for the billing service." links={links}>
-            <Subscriptions products={["chat.chat"]} />
+            <Subscriptions products={["chat.conversation.chat"]} />
         </SubAppShell>
     );
 }
--- packages/ui-storage/src/BillingPage/index.tsx
+++ packages/ui-storage/src/BillingPage/index.tsx
@@ -7,7 +7,7 @@ interface IProps {}
 export function BillingPage({}: IProps) {
     return (
         <SubAppShell title="Storage / Billing" description="Subscribe and manage your subscriptions for the storage service." links={links}>
-            <Subscriptions products={["storage.document.process.text"]} />
+            <Subscriptions products={["storage.collection.document.process"]} />
         </SubAppShell>
     );
 }
