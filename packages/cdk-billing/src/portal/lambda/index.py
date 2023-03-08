c91762b0e3d5b2e965813773a8f2e543c6d43024
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-03-08 18:00:09 +1100

    Updated payment portal

--- packages/cdk-billing/src/portal/lambda/index.py
+++ packages/cdk-billing/src/portal/lambda/index.py
@@ -39,62 +39,120 @@ def lambda_handler(event, context):
     home_url = os.getenv("HOME_URL")
 
     user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
-    
-    query_params = event["multiValueQueryStringParameters"]
 
-    product_ids = None
-    if query_params != None and "productId" in query_params:
-        product_ids = query_params["productId"]
+    # Load the Stripe data
+    stripe_data = json.loads(secrets_manager_client.get_secret_value(SecretId=stripe_secret)["SecretString"])
+    stripe.api_key = stripe_data["apiKey"]
 
-    # Load the Stripe key
-    stripe.api_key = secrets_manager_client.get_secret_value(SecretId=stripe_secret)["SecretString"]
+    stripe_product_id = stripe_data["productId"]
+    stripe_price_id = stripe_data["priceId"]
 
     # Retrieve customer account
     customer_item = dynamodb_client.get_item(TableName=user_billing_table, Key={"userId": {"S": user_id}})["Item"]
     customer_id = customer_item["stripeCustomerId"]["S"]
     sandbox_mode = customer_item["sandbox"]["BOOL"] if "sandbox" in customer_item else False
 
-    # Retrieve the product
-    if product_ids == None:
-        return route_to_portal(customer_id, home_url, user_id)
+    if sandbox_mode:
+        logger.info(f"User is in sandbox mode")
 
-    # Check if the customer already has a subscription to the provided product ids
-    customer = stripe.Customer.retrieve(customer_id, expand=["subscriptions"])
-    subscription_items = customer["subscriptions"]["data"]
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps({
+                "url": session["url"],
+                "active": sandbox_mode,
+                "status": "SANDBOX"
+            })
+        }
 
-    subscribed_ids = set()
-    price_mappings = {}
+    # Get account active and status states
+    active = False
+    status = None
 
-    for product_id in product_ids:
-        product_item = dynamodb_client.get_item(TableName=products_table, Key={"productId": {"S": product_id}})["Item"]
+    subscriptions = stripe.Subscription.list(customer=customer_id).data
 
-        stripe_product_id = product_item["stripeProductId"]["S"]
-        stripe_price_id = product_item["stripePriceId"]["S"]
+    active_subscriptions = []
+    other_subscriptions = []
 
-        price_mappings[product_id] = stripe_price_id
+    for subscription in subscriptions:
+        if subscription.items.data[0].product.id == stripe_product_id:
+            if subscription.status == "active":
+                active_subscriptions.append(subscription)
+            else:
+                other_subscriptions.append(subscription)
 
-        for subscription_item in subscription_items:
-            if subscription_item["items"]["data"][0]["price"]["product"] == stripe_product_id:
-                subscribed_ids.add(product_id)
-                
-                break
+    if active_subscriptions:
+        # Customer has an active subscription to the given product ID
+        subscription = active_subscriptions[0]
+        has_card = bool(subscription.default_payment_method)
+        is_trialing = subscription.status == "trialing"
 
+        if is_trialing:
+            if has_card:
+                # Customer is on a trial and has added a card
+                active = True
+                status = "TRIALING_CARD"
+            else:
+                # Customer is on a trial but has not added a card
+                active = True
+                status = "TRIALING_NO_CARD"
+        else:
+            # Customer has an active subscription
+            active = True
+            status = "CARD"
 
-    if len(subscribed_ids) == len(product_ids):
-        return route_to_portal(customer_id, home_url, user_id)
+    else:
+        if other_subscriptions:
+            # Customer has subscribed to the given product ID before but has since cancelled
+            active = False
+            status = "NOT_SUBSCRIBED_TRIAL_ENDED"
+        else:
+            # Customer has never subscribed to the given product ID
+            active = False
+            status = "NOT_SUBSCRIBED"
+
+    if active:
+        portal = stripe.billing_portal.Session.create(
+            customer=customer_id,
+            return_url=home_url
+        )
+
+        logger.info(f"Created portal session for user `{user_id}` with active '{active}' and status '{status}'")
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps({
+                "url": portal["url"],
+                "active": active,
+                "status": status
+            })
+        }
 
     # Route to checkout
-    line_items = [{"price": price_mappings[product_id]} for product_id in product_ids if product_id not in subscribed_ids]
+    subscription_data = {
+        "subscription_data": {
+            "trial_settings": {"end_behavior": {"missing_payment_method": "cancel"}},
+            "trial_period_days": 30,
+        },
+        "payment_method_collection": "if_required"
+    } if status == "NOT_SUBSCRIBED" else {}
+
 
     session = stripe.checkout.Session.create(
         success_url=f"{home_url}?status=SUCCESS",
         cancel_url=f"{home_url}?status=FAILED",
-        line_items=line_items,
+        line_items=[{"price": stripe_price_id}],
         mode="subscription",
-        customer=customer_id
+        customer=customer_id,
+        **subscription_data
     )
 
-    logger.info(f"Created checkout session for user `{user_id}`")
+    logger.info(f"Created checkout session for user `{user_id}` with active '{active}' and status '{status}'")
 
     return {
         "statusCode": 200,
@@ -103,7 +161,8 @@ def lambda_handler(event, context):
         },
         "body": json.dumps({
             "url": session["url"],
-            "active": sandbox_mode
+            "active": active,
+            "status": status
         })
     }
 
